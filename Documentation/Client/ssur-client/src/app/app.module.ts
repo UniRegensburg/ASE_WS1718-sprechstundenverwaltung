153a4c07169141780d90c6c47823c9aa48bba221
PWinderl <paul.winderl@stud.uni-regensburg.de>
2018-01-30 11:47:18 +0100

    Initial Commit Calendar #5
    Implemented fullcalendar.io calendar; Added material design css;
    Stripped calendar from weekends; Defined necessary time range;

--- Documentation/Client/ssur-client/src/app/app.module.ts
+++ Documentation/Client/ssur-client/src/app/app.module.ts
@@ -1,25 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { BrowserAnimationsModule} from "@angular/platform-browser/animations";
+import { BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
-import { FormsModule} from "@angular/forms";
-import { MatAutocompleteModule} from "@angular/material";
-import { ReactiveFormsModule} from "@angular/forms";
-import { MatFormFieldModule} from "@angular/material";
-import { MatInputModule} from "@angular/material";
-import { MatSlideToggleModule} from "@angular/material";
-import { HttpClientModule} from "@angular/common/http";
-import { ProfessorService} from "./services/ProfessorService";
-import {HttpModule} from "@angular/http";
+import { FormsModule} from '@angular/forms';
+import { MatAutocompleteModule} from '@angular/material';
+import { ReactiveFormsModule} from '@angular/forms';
+import { MatFormFieldModule} from '@angular/material';
+import { MatInputModule} from '@angular/material';
+import { MatSlideToggleModule} from '@angular/material';
+import { HttpClientModule} from '@angular/common/http';
+import { ProfessorService} from './services/ProfessorService';
+import {HttpModule} from '@angular/http';
+import { CalendarModule} from 'ap-angular2-fullcalendar';
 
 
 import { AppComponent } from './app.component';
 import { ProfessorSelectorComponent } from './professor-selector/professor-selector.component';
+import { MainCalComponent } from './main-cal/main-cal.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProfessorSelectorComponent
+    ProfessorSelectorComponent,
+    MainCalComponent
   ],
   imports: [
     BrowserModule,
@@ -31,7 +34,8 @@ import { ProfessorSelectorComponent } from './professor-selector/professor-selec
     MatInputModule,
     MatSlideToggleModule,
     HttpClientModule,
-    HttpModule
+    HttpModule,
+    CalendarModule
   ],
   providers: [ProfessorService],
   bootstrap: [AppComponent]
---
+++ Documentation/Client/ssur-client/src/app/main-cal/main-cal.component.ts
@@ -0,0 +1,93 @@
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { CalendarComponent} from 'ap-angular2-fullcalendar';
+
+@Component({
+  selector: 'app-main-cal',
+  templateUrl: './main-cal.component.html',
+  styleUrls: ['./main-cal.component.css']
+})
+export class MainCalComponent implements OnInit {
+
+  @ViewChild(CalendarComponent) myCalendar: CalendarComponent;
+
+  calendarOptions: Object = {
+
+    editable: false,
+    handleWindowResize: true,
+    weekends: false,
+    defaultView: 'agendaWeek',
+    minTime: '08:00:00',
+    maxTime: '20:00:00',
+    columnFormat: 'ddd D/M',
+    timeFormat: 'HH:mm',
+    displayEventTime: true,
+    allDayText: 'Ganztägig',
+
+    events: [
+      {
+        title: 'All Day Event',
+        start: '2016-09-01'
+      },
+      {
+        title: 'Long Event',
+        start: '2016-09-07',
+        end: '2016-09-10'
+      },
+      {
+        id: 999,
+        title: 'Repeating Event',
+        start: '2016-09-09T16:00:00'
+      },
+      {
+        id: 999,
+        title: 'Repeating Event',
+        start: '2016-09-16T16:00:00'
+      },
+      {
+        title: 'Conference',
+        start: '2016-09-11',
+        end: '2016-09-13'
+      },
+      {
+        title: 'Meeting',
+        start: '2016-09-12T10:30:00',
+        end: '2016-09-12T12:30:00'
+      },
+      {
+        title: 'Lunch',
+        start: '2016-09-12T12:00:00'
+      },
+      {
+        title: 'Meeting',
+        start: '2016-09-12T14:30:00'
+      },
+      {
+        title: 'Happy Hour',
+        start: '2016-09-12T17:30:00'
+      },
+      {
+        title: 'Dinner',
+        start: '2016-09-12T20:00:00'
+      },
+      {
+        title: 'Birthday Party',
+        start: '2016-09-13T07:00:00'
+      },
+      {
+        title: 'Click for Google',
+        url: 'http://google.com/',
+        start: '2016-09-28'
+      }
+    ]
+  };
+
+  onCalendarInit(initialized: boolean) {
+    console.log('Calendar initialized');
+  }
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
