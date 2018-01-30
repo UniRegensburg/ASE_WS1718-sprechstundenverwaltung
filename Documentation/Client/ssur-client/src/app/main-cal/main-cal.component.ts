d1ba95708f387a9dc39cf575becb05e5cdefb274
PWinderl <paul.winderl@stud.uni-regensburg.de>
2018-01-30 20:00:20 +0100

    Structuring code and adding own event #5
    Code is now more structured; Own event is now pushed in events;

--- Documentation/Client/ssur-client/src/app/main-cal/main-cal.component.ts
+++ Documentation/Client/ssur-client/src/app/main-cal/main-cal.component.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import { CalendarComponent} from 'ap-angular2-fullcalendar';
+import * as $ from 'jquery';
+import * as moment from 'moment';
 
 @Component({
   selector: 'app-main-cal',
@@ -8,9 +10,38 @@ import { CalendarComponent} from 'ap-angular2-fullcalendar';
 })
 export class MainCalComponent implements OnInit {
 
-  @ViewChild(CalendarComponent) myCalendar: CalendarComponent;
+  calendarOptions = {
 
-  calendarOptions: Object = {
+    editable: false,
+    handleWindowResize: true,
+    weekends: false,
+    defaultView: 'agendaWeek',
+    minTime: '08:00:00',
+    maxTime: '20:00:00',
+    columnFormat: 'ddd D/M',
+    timeFormat: 'HH:mm',
+    displayEventTime: true,
+    allDayText: 'Ganztägig',
+
+    events: []};
+
+  myEvent = {
+    title: 'Pauls Event',
+    allDay: false,
+    start: '2018-01-31T16:00:00',
+    end: '2018-01-31T76:00:00'
+  };
+
+  @ViewChild(CalendarComponent) myCalendar: CalendarComponent;
+  // @ViewChild('myCalendar', {read: ElementRef}) myCalendar: ElementRef;
+
+  changeCalendarView(view) {
+
+    this.myCalendar.fullCalendar('changeView', view);
+
+  }
+
+   /* calendarOptions: Object = {
 
     editable: false,
     handleWindowResize: true,
@@ -56,38 +87,73 @@ export class MainCalComponent implements OnInit {
       {
         title: 'Lunch',
         start: '2016-09-12T12:00:00'
-      },
-      {
-        title: 'Meeting',
-        start: '2016-09-12T14:30:00'
-      },
-      {
-        title: 'Happy Hour',
-        start: '2016-09-12T17:30:00'
-      },
-      {
-        title: 'Dinner',
-        start: '2016-09-12T20:00:00'
-      },
-      {
-        title: 'Birthday Party',
-        start: '2016-09-13T07:00:00'
-      },
-      {
-        title: 'Click for Google',
-        url: 'http://google.com/',
-        start: '2016-09-28'
       }
     ]
-  };
-
-  onCalendarInit(initialized: boolean) {
-    console.log('Calendar initialized');
-  }
+  };*/
 
   constructor() { }
 
   ngOnInit() {
+    /*this.calendarOptions = {
+
+      editable: false,
+      handleWindowResize: true,
+      weekends: false,
+      defaultView: 'agendaWeek',
+      minTime: '08:00:00',
+      maxTime: '20:00:00',
+      columnFormat: 'ddd D/M',
+      timeFormat: 'HH:mm',
+      displayEventTime: true,
+      allDayText: 'Ganztägig',*/
+
+      const newEvents = [
+          {
+            title: 'All Day Event',
+            start: '2016-09-01'
+          },
+          {
+            title: 'Long Event',
+            start: '2016-09-07',
+            end: '2016-09-10'
+          },
+          {
+            id: 999,
+            title: 'Repeating Event',
+            start: '2016-09-09T16:00:00'
+          },
+          {
+            id: 999,
+            title: 'Repeating Event',
+            start: '2016-09-16T16:00:00'
+          },
+          {
+            title: 'Conference',
+            start: '2018-01-30T08:00:00',
+            end: '2018-01-30T09:30:00'
+          },
+          {
+            title: 'Meeting',
+            start: '2016-09-12T10:30:00',
+            end: '2016-09-12T12:30:00'
+          },
+          {
+            title: 'Lunch',
+            start: '2016-09-12T12:00:00'
+          },
+        ];
+      newEvents.push(this.myEvent);
+        this.calendarOptions.events = newEvents;
+        this.myCalendar.fullCalendar('renderEvents', newEvents, true);
   }
+
+ /* changeCalendarView(view) {
+    this.myCalendar.fullCalendar('changeView', view);
+  }*/
+
+
+ /* onCalendarInit(initialized: boolean); {
+    console.log('Calendar initialized');
+  }*/
 
 }
