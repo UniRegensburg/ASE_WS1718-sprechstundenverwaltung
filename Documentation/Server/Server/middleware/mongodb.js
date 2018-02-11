0bedd6029cffae3c73d6c88f58847f21a7f0792a
Felix Riedl <felix1.riedl@stud.uni-regensburg.de>
2018-02-11 11:13:46 +0100

    Replace MongoClient with Mongoose Models

--- Documentation/Server/Server/middleware/mongodb.js
+++ Documentation/Server/Server/middleware/mongodb.js
@@ -1,9 +1,17 @@
-const MongoClient = require('mongodb').MongoClient;
+//const MongoClient = require('mongodb').MongoClient;
 const URI = require('../config/security').uriMongo;
+var User = require('../models/models').User;
+var  Meeting = require('../models/models').Meeting;
+var Professor = require('../models/models').Professor;
+
+const mongoose = require('mongoose', function(err){
+    console.log(err);
+});
+mongoose.connect(URI);
 
 module.exports = {
     testDatabase: function(req, res, next){
-        MongoClient.connect(URI, function(err, db) {
+        /*MongoClient.connect(URI, function(err, db) {
             if(err) this.sendDatabaseError(res, err);
             var dbo = db.db("ASE1718");
             dbo.collection("users").find({"role":"professor"}).toArray(function(err, result){
