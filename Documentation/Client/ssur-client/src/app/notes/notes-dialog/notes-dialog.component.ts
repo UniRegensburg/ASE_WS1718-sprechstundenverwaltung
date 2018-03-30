6f874e7e2e4f75178f966f820588e5eb01a22670
Ramona <ramona.voelkel@stud.uni-regensburg.de>
2018-03-30 12:17:51 +0200

    init file upload service

--- Documentation/Client/ssur-client/src/app/notes/notes-dialog/notes-dialog.component.ts
+++ Documentation/Client/ssur-client/src/app/notes/notes-dialog/notes-dialog.component.ts
@@ -3,23 +3,41 @@ import { MatDialogRef,MatDialog, MAT_DIALOG_DATA} from '@angular/material';
 import {NotesComponent} from "../notes.component";
 import {NotesService} from "../../services/notes.service";
 import {NgModel} from "@angular/forms";
+import {FileUploadService} from "../../services/file-upload.service";
 
 @Component({
   selector: 'app-notes-dialog',
   templateUrl: './notes-dialog.component.html',
-  styleUrls: ['./notes-dialog.component.css']
+  styleUrls: ['./notes-dialog.component.css'],
+  providers: [FileUploadService]
 })
 export class NotesDialogComponent implements OnInit {
-  
+  fileToUpload: File = null;
+
   constructor(
     public dialogRef: MatDialogRef<NotesDialogComponent>,
-    @Inject(MAT_DIALOG_DATA) public data: any) { }
+    @Inject(MAT_DIALOG_DATA) public data: any,
+    public fileUploadService: FileUploadService) { }
 
   ngOnInit() {
   }
 
   onNoClick(): void {
     this.dialogRef.close();
+  }
+
+
+  handleFileInput(files: FileList) {
+      this.fileToUpload = files.item(0);
+      this.uploadFileToActivity();
+  }
+
+  uploadFileToActivity() {
+    this.fileUploadService.postFile(this.fileToUpload).subscribe(data => {
+      // do something, if upload success
+    }, error => {
+      console.log(error);
+    });
   }
 
 }
