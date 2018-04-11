380758249285aaf2a7511d9df7ea0be99641b8a3
Ramona <ramona.voelkel@stud.uni-regensburg.de>
2018-04-11 17:20:19 +0200

    code cleaning

--- Documentation/Client/ssur-client/src/app/notes/notes.component.ts
+++ Documentation/Client/ssur-client/src/app/notes/notes.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit} from '@angular/core';
-import {MatDialog, MatDialogRef, MAT_DIALOG_DATA} from '@angular/material/dialog';
-import {NotesDialogComponent} from "../dialogs/notes-dialog/notes-dialog.component";
-import {NotesService} from "../services/notes.service";
+import {MatDialog, MatDialogRef} from '@angular/material/dialog';
+import {NotesDialogComponent} from '../dialogs/notes-dialog/notes-dialog.component';
+import {NotesService} from '../services/notes.service';
 
 
 @Component({
@@ -13,7 +13,7 @@ import {NotesService} from "../services/notes.service";
 export class NotesComponent implements OnInit {
 
   note: string;
-  id = '5ac0fccbfb910820e064fada' ; //todo: use actual id as parameter, not constant value of dummy-conversation
+  id = '5ac0fccbfb910820e064fada' ; // todo: use actual id as parameter, not constant value of dummy-conversation
   notes = this.notesService.getNotes(this.id);
   NotesDialogRef: MatDialogRef<NotesDialogComponent>;
 
@@ -31,8 +31,9 @@ export class NotesComponent implements OnInit {
 
       this.NotesDialogRef.afterClosed().subscribe(result => {
         this.note = result;
-        if (this.note != undefined){
-          this.notesService.setNotes(this.note, this.id);}
+        if (this.note !== undefined) {
+          this.notesService.setNotes(this.note, this.id);
+        }
       });
   }
 }
